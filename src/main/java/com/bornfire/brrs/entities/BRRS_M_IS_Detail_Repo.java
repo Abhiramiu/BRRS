133cc6075f2944903abfbcda93415daebb48a49f
Gopika <gopikabharathi15@gmail.com>
2025-11-22 17:44:49 +0530

    M_IS Onclick

--- src/main/java/com/bornfire/brrs/entities/BRRS_M_IS_Detail_Repo.java
+++ src/main/java/com/bornfire/brrs/entities/BRRS_M_IS_Detail_Repo.java
@@ -20,8 +20,8 @@ public interface BRRS_M_IS_Detail_Repo extends JpaRepository<M_IS_Detail_Entity,
     int getdatacount(Date reportdate);
 
    
-	@Query(value = "select * from BRRS_M_IS_DETAILTABLE where ROW_ID =?1 and COLUMN_ID=?2 AND REPORT_DATE=?3", nativeQuery = true)
-	List<M_IS_Detail_Entity> GetDataByRowIdAndColumnId(String rowId,String ColumnId,Date reportdate);
+	@Query(value = "select * from BRRS_M_IS_DETAILTABLE where REPORT_LABEL =?1 and REPORT_ADDL_CRITERIA_1=?2 AND REPORT_DATE=?3", nativeQuery = true)
+	List<M_IS_Detail_Entity> GetDataByRowIdAndColumnId(String reportLabel,String reportAddlCriteria_1,Date reportdate);
 	
 	
 }
