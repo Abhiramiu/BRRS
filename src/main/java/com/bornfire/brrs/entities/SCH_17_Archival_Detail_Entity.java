b621de0aad77d1515d2b8cebb36849ca56d3e4f5
ishwaryalakshmi <ishwaryaishu0103@gmail.com>
2026-01-07 17:04:31 -0800

    SCH17   CHANGES

--- src/main/java/com/bornfire/brrs/entities/SCH_17_Archival_Detail_Entity.java
+++ src/main/java/com/bornfire/brrs/entities/SCH_17_Archival_Detail_Entity.java
@@ -17,250 +17,252 @@ public class SCH_17_Archival_Detail_Entity {
 
    
 	
-	 @Column(name = "CUST_ID")
-  private String custId;
-	 @Id
-  @Column(name = "ACCT_NUMBER")
-  private String acctNumber;
+	@Id
+	@Column(name = "SNO")
+	private Long sno;
 
-  @Column(name = "ACCT_NAME")
-  private String acctName;
+	@Column(name = "CUST_ID")
+	private String custId;
 
+	@Column(name = "ACCT_NUMBER")
+	private String acctNumber;
 
-  @Column(name = "DATA_TYPE")
-  private String dataType;
+	@Column(name = "ACCT_NAME")
+	private String acctName;
 
-  @Column(name = "REPORT_NAME")
-  private String reportName;
-  
-  @Column(name = "REPORT_LABEL")
-  private String reportLabel;
-  
-  @Column(name = "REPORT_ADDL_CRITERIA_1")
-  private String reportAddlCriteria_1;
-  
- 
-  
-  @Column(name = "REPORT_REMARKS")
-  private String reportRemarks;
-  
+	@Column(name = "DATA_TYPE")
+	private String dataType;
 
+	@Column(name = "REPORT_NAME")
+	private String reportName;
 
-  @Column(name = "MODIFICATION_REMARKS")
-  private String modificationRemarks;
+	@Column(name = "REPORT_LABEL")
+	private String reportLabel;
 
-  @Column(name = "DATA_ENTRY_VERSION")
-  private String dataEntryVersion;
+	@Column(name = "REPORT_ADDL_CRITERIA_1")
+	private String reportAddlCriteria_1;
 
-  @Column(name = "ACCT_BALANCE_IN_PULA", precision = 24, scale = 3)
-  private BigDecimal acctBalanceInpula;
+	@Column(name = "REPORT_REMARKS")
+	private String reportRemarks;
 
+	@Column(name = "MODIFICATION_REMARKS")
+	private String modificationRemarks;
 
-  @Column(name = "REPORT_DATE")
-  @DateTimeFormat(pattern = "dd-MM-yyyy")
-  private Date reportDate;
+	@Column(name = "DATA_ENTRY_VERSION")
+	private String dataEntryVersion;
 
+	@Column(name = "ACCT_BALANCE_IN_PULA", precision = 24, scale = 3)
+	private BigDecimal acctBalanceInpula;
 
-  @Column(name = "CREATE_USER")
-  private String createUser;
+	@Column(name = "REPORT_DATE")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date reportDate;
 
-  @Column(name = "CREATE_TIME")
-  @DateTimeFormat(pattern = "dd-MM-yyyy")
-  private Date createTime;
+	@Column(name = "CREATE_USER")
+	private String createUser;
 
-  @Column(name = "MODIFY_USER")
-  private String modifyUser;
+	@Column(name = "CREATE_TIME")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date createTime;
 
+	@Column(name = "MODIFY_USER")
+	private String modifyUser;
 
-  @Column(name = "MODIFY_TIME")
-  @DateTimeFormat(pattern = "dd-MM-yyyy")
-  private Date modifyTime;
+	@Column(name = "MODIFY_TIME")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date modifyTime;
 
-  @Column(name = "VERIFY_USER")
-  private String verifyUser;
+	@Column(name = "VERIFY_USER")
+	private String verifyUser;
 
+	@Column(name = "VERIFY_TIME")
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date verifyTime;
 
-  @Column(name = "VERIFY_TIME")
-  @DateTimeFormat(pattern = "dd-MM-yyyy")
-  private Date verifyTime;
+	@Column(name = "ENTITY_FLG")
+	private Character entityFlg;
 
-  @Column(name = "ENTITY_FLG")
-  private char entityFlg;
+	@Column(name = "MODIFY_FLG")
+	private Character modifyFlg;
 
-  @Column(name = "MODIFY_FLG")
-  private char modifyFlg;
+	@Column(name = "DEL_FLG")
+	private Character delFlg;
 
-  @Column(name = "DEL_FLG")
-  private char delFlg;
+	public String getCustId() {
+		return custId;
+	}
 
-public String getCustId() {
-	return custId;
-}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
 
-public void setCustId(String custId) {
-	this.custId = custId;
-}
+	public String getAcctNumber() {
+		return acctNumber;
+	}
 
-public String getAcctNumber() {
-	return acctNumber;
-}
+	public void setAcctNumber(String acctNumber) {
+		this.acctNumber = acctNumber;
+	}
 
-public void setAcctNumber(String acctNumber) {
-	this.acctNumber = acctNumber;
-}
+	public String getAcctName() {
+		return acctName;
+	}
 
-public String getAcctName() {
-	return acctName;
-}
+	public void setAcctName(String acctName) {
+		this.acctName = acctName;
+	}
 
-public void setAcctName(String acctName) {
-	this.acctName = acctName;
-}
+	public String getDataType() {
+		return dataType;
+	}
 
-public String getDataType() {
-	return dataType;
-}
-
-public void setDataType(String dataType) {
-	this.dataType = dataType;
-}
-
-public String getReportName() {
-	return reportName;
-}
-
-public void setReportName(String reportName) {
-	this.reportName = reportName;
-}
-
-public String getReportLabel() {
-	return reportLabel;
-}
-
-public void setReportLabel(String reportLabel) {
-	this.reportLabel = reportLabel;
-}
-
-public String getReportAddlCriteria_1() {
-	return reportAddlCriteria_1;
-}
-
-public void setReportAddlCriteria_1(String reportAddlCriteria_1) {
-	this.reportAddlCriteria_1 = reportAddlCriteria_1;
-}
-
-public String getReportRemarks() {
-	return reportRemarks;
-}
-
-public void setReportRemarks(String reportRemarks) {
-	this.reportRemarks = reportRemarks;
-}
-
-public String getModificationRemarks() {
-	return modificationRemarks;
-}
-
-public void setModificationRemarks(String modificationRemarks) {
-	this.modificationRemarks = modificationRemarks;
-}
-
-public String getDataEntryVersion() {
-	return dataEntryVersion;
-}
-
-public void setDataEntryVersion(String dataEntryVersion) {
-	this.dataEntryVersion = dataEntryVersion;
-}
-
-public BigDecimal getAcctBalanceInpula() {
-	return acctBalanceInpula;
-}
-
-public void setAcctBalanceInpula(BigDecimal acctBalanceInpula) {
-	this.acctBalanceInpula = acctBalanceInpula;
-}
-
-public Date getReportDate() {
-	return reportDate;
-}
-
-public void setReportDate(Date reportDate) {
-	this.reportDate = reportDate;
-}
-
-public String getCreateUser() {
-	return createUser;
-}
-
-public void setCreateUser(String createUser) {
-	this.createUser = createUser;
-}
-
-public Date getCreateTime() {
-	return createTime;
-}
-
-public void setCreateTime(Date createTime) {
-	this.createTime = createTime;
-}
-
-public String getModifyUser() {
-	return modifyUser;
-}
-
-public void setModifyUser(String modifyUser) {
-	this.modifyUser = modifyUser;
-}
-
-public Date getModifyTime() {
-	return modifyTime;
-}
-
-public void setModifyTime(Date modifyTime) {
-	this.modifyTime = modifyTime;
-}
-
-public String getVerifyUser() {
-	return verifyUser;
-}
-
-public void setVerifyUser(String verifyUser) {
-	this.verifyUser = verifyUser;
-}
-
-public Date getVerifyTime() {
-	return verifyTime;
-}
-
-public void setVerifyTime(Date verifyTime) {
-	this.verifyTime = verifyTime;
-}
-
-public char getEntityFlg() {
-	return entityFlg;
-}
-
-public void setEntityFlg(char entityFlg) {
-	this.entityFlg = entityFlg;
-}
-
-public char getModifyFlg() {
-	return modifyFlg;
-}
-
-public void setModifyFlg(char modifyFlg) {
-	this.modifyFlg = modifyFlg;
-}
-
-public char getDelFlg() {
-	return delFlg;
-}
-
-public void setDelFlg(char delFlg) {
-	this.delFlg = delFlg;
-}
-
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	public String getReportName() {
+		return reportName;
+	}
+
+	public void setReportName(String reportName) {
+		this.reportName = reportName;
+	}
+
+	public String getReportLabel() {
+		return reportLabel;
+	}
+
+	public void setReportLabel(String reportLabel) {
+		this.reportLabel = reportLabel;
+	}
+
+	public String getReportAddlCriteria_1() {
+		return reportAddlCriteria_1;
+	}
+
+	public void setReportAddlCriteria_1(String reportAddlCriteria_1) {
+		this.reportAddlCriteria_1 = reportAddlCriteria_1;
+	}
+
+	public String getReportRemarks() {
+		return reportRemarks;
+	}
+
+	public void setReportRemarks(String reportRemarks) {
+		this.reportRemarks = reportRemarks;
+	}
+
+	public String getModificationRemarks() {
+		return modificationRemarks;
+	}
+
+	public void setModificationRemarks(String modificationRemarks) {
+		this.modificationRemarks = modificationRemarks;
+	}
+
+	public String getDataEntryVersion() {
+		return dataEntryVersion;
+	}
+
+	public void setDataEntryVersion(String dataEntryVersion) {
+		this.dataEntryVersion = dataEntryVersion;
+	}
+
+	public BigDecimal getAcctBalanceInpula() {
+		return acctBalanceInpula;
+	}
+
+	public void setAcctBalanceInpula(BigDecimal acctBalanceInpula) {
+		this.acctBalanceInpula = acctBalanceInpula;
+	}
+
+	public Date getReportDate() {
+		return reportDate;
+	}
+
+	public void setReportDate(Date reportDate) {
+		this.reportDate = reportDate;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getModifyUser() {
+		return modifyUser;
+	}
+
+	public void setModifyUser(String modifyUser) {
+		this.modifyUser = modifyUser;
+	}
+
+	public Date getModifyTime() {
+		return modifyTime;
+	}
+
+	public void setModifyTime(Date modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+
+	public String getVerifyUser() {
+		return verifyUser;
+	}
+
+	public void setVerifyUser(String verifyUser) {
+		this.verifyUser = verifyUser;
+	}
+
+	public Date getVerifyTime() {
+		return verifyTime;
+	}
+
+	public void setVerifyTime(Date verifyTime) {
+		this.verifyTime = verifyTime;
+	}
+
+	public Character getEntityFlg() {
+		return entityFlg;
+	}
+
+	public void setEntityFlg(Character entityFlg) {
+		this.entityFlg = entityFlg;
+	}
+
+	public Character getModifyFlg() {
+		return modifyFlg;
+	}
+
+	public void setModifyFlg(Character modifyFlg) {
+		this.modifyFlg = modifyFlg;
+	}
+
+	public Character getDelFlg() {
+		return delFlg;
+	}
+
+	public void setDelFlg(Character delFlg) {
+		this.delFlg = delFlg;
+	}
+
+	public Long getSno() {
+		return sno;
+	}
+
+	public void setSno(Long sno) {
+		this.sno = sno;
+	}
 public SCH_17_Archival_Detail_Entity() {
 	super();
 	// TODO Auto-generated constructor stub
