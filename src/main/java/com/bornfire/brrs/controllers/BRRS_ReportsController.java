74924b497ccec1c868c7f9071f93d7c6afe88b55
ishwaryalakshmi <ishwaryaishu0103@gmail.com>
2026-01-24 17:53:16 -0800

    Update BRRS_ReportsController.java

--- src/main/java/com/bornfire/brrs/controllers/BRRS_ReportsController.java
+++ src/main/java/com/bornfire/brrs/controllers/BRRS_ReportsController.java
@@ -1772,27 +1772,7 @@ public class BRRS_ReportsController {
 	
 	
 	
-	@Autowired
-	BRRS_M_INT_RATES_ReportService INT_RATES_reportService;
 
-	@PostMapping("/INTRATESupdateAll")
-	@ResponseBody
-	public ResponseEntity<String> updateINTRATESAllReports(
-			@RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") Date asondate,
-			@RequestParam Map<String, String> allParams) {
-		try {
-			System.out.println("Came to INT_RATES controller");
-
-			INT_RATES_reportService.updateDetailFromForm(asondate, allParams);
-
-			return ResponseEntity.ok("Updated Successfully.");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity
-					.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body("Update Failed: " + e.getMessage());
-		}
-	}
 	
 	
 	@Autowired
